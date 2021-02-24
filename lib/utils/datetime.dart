6efd569fa973328889b8688277e0e6a0ef56d624
goandreus <andres.chavez.crisostomo@gmail.com>
2021-02-23 21:36:48 -0500

    utils details (#5)

---
+++ lib/utils/datetime.dart
@@ -0,0 +1,13 @@
+DateTime getDateTime(int timestampInSeconds, int timezone) {
+  return DateTime.fromMillisecondsSinceEpoch(timestampInSeconds * 1000,
+          isUtc: true)
+      .add(Duration(seconds: timezone));
+}
+
+double getPercentCover(
+    {DateTime dateTime, DateTime minDateTime, DateTime maxDateTime}) {
+  if (dateTime.isBefore(minDateTime)) return 0.0;
+  if (dateTime.isAfter(maxDateTime)) return 1.0;
+  return dateTime.difference(minDateTime).inSeconds /
+      maxDateTime.difference(minDateTime).inSeconds;
+}
